ad8655d9482336a087e9e8a0fa0437a12117c6fd
Ifrah Dahir <ifrahdahir@Ifrahs-MacBook-Air.local>
2022-09-07 21:21:37 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const slices = 6;
+const pizza = `There are ${slices} in a medium boxes`
+console.log(pizza)
+
+
+let drinkA = 4.50
+let drinkB = 6.50
+console.log{"The cost of the two drinks is" + (drinkA + drinkB) + "dollars"}
+
+
+
+let drink1 = 2.30;
+let drink2 = 2.20;
+let bill = `The total cost of your drinks is ${drink1 + drink2}
+
+console.log(bill)
+
+
+
+if (number > 0) {
+    console.log("The number is positive")
+  } else {
+    console.log("The number is NOT positive")
+  }
+
+  //ternary operator 
+
+  let number = > 0
+  let result = ("number > 0 be a positive" : "number < 0 Not a positive")
